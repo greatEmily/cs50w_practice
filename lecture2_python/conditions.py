d6ba8e050b2933c11f50a9ad319da6b4da897c88
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2025-10-14 20:53:30 +0000

    add functions, conditions, sequences

---
+++ lecture2_python/conditions.py
@@ -0,0 +1,8 @@
+n = int(input("Number: "))
+
+if n > 0:
+    print("number is positive")
+elif n < 0:
+    print("number is negative")
+else:
+    print("number is zero")
---
+++ lecture2_python/sequences.py
@@ -0,0 +1,3 @@
+name = "Emily"
+
+print(name[0])
