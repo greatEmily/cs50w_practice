19c790dc7da065ab222617daeac3511b261902ea
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2025-10-19 20:17:44 +0000

    adding priority to the form

--- lecture3_django/lecture3/tasks/views.py
+++ lecture3_django/lecture3/tasks/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django import forms
 
 tasks = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task", max_length=100)
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return render(request, "tasks/index.html", {"tasks": tasks})
+        else:
+            return render(request, "tasks/add.html", {"form": form})
+    return render(request, "tasks/add.html"), {
+        "form": NewTaskForm()
+    }
